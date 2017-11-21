9fafeee8b18d8ced37227c010c05536ec26812e7
vdiez <diez@outlook.com>
2017-11-21 11:22:10 +0100

    improved verbosity added parameter checks

--- index.js
+++ index.js
@@ -3,42 +3,56 @@ let winston = require('winston');
 
 function Queue(limit) {
     this.queues = new Array(limit).fill(1).map((_, idx) => idx);
-    this.arbiter = 0;
+    this.dispatcher = 0;
+    this.tasks = 0;
 }
 
 Queue.prototype.run = function (task) {
     let self = this;
-    self.arbiter = Promise.resolve(self.arbiter)
-        .then(function () {
-            return new Promise(function (resolve, reject) {
-                return Promise.race(self.queues)
-                    .then(function (queue) {
-                        self.queues[queue] = Promise.resolve(self.queues[queue])
-                            .then(function () {
-                                return task();
-                            })
-                            .catch(err => {
-                                winston.error((task.name || task) + " failed on queue " + queue + ". Error: ", err);
-                            })
-                            .then(function () {
-                                winston.info((task.name || task) + " completed on queue " + queue);
-                                return queue;
-                            });
-                        resolve();
-                    })
-            });
-        })
+    self.tasks++;
+    winston.silly((task.name || task) + " added. Pending tasks: " + self.tasks);
+    return new Promise((resolve, reject) => {
+        self.dispatcher = Promise.resolve(self.dispatcher)
+            .then(function () {
+                return new Promise(function (resolve_dispatcher) {
+                    return Promise.race(self.queues)
+                        .then(function (queue) {
+                            self.queues[queue] = Promise.resolve(self.queues[queue])
+                                .then(function () {
+                                    return task();
+                                })
+                                .catch(err => {
+                                    winston.error((task.name || task) + " failed on queue " + queue + ". Error: ", err);
+                                    reject(err);
+                                })
+                                .then(result => {
+                                    self.tasks--;
+                                    winston.info((task.name || task) + " completed on queue " + queue + ". Pending tasks: " + self.tasks);
+                                    resolve(result);
+                                    return queue;
+                                });
+                            resolve_dispatcher();
+                        })
+                });
+            })
+    });
 };
 
 module.exports = function(name, limit) {
-    if (typeof name === "number") {
-        limit = name;
-        name = '';
+    if (typeof limit === "undefined") {
+        if (typeof name === "number") {
+            limit = name;
+            name = '';
+        }
+        else if (typeof name === "undefined") name = '';
+        if (typeof name !== "string") throw "Incorrect parameters";
     }
+    else limit = +limit;
 
-    if (typeof name === "string" && typeof limit === "number") {
-        if (!queues.hasOwnProperty(name)) queues[name] = new Queue(limit);
-        return queues[name];
+    name = name + '';
+    if (!queues.hasOwnProperty(name)) {
+        if (isNaN(limit) || limit < 1) throw "Incorrect parameters";
+        queues[name] = new Queue(limit);
     }
-    else throw "Incorrect parameters";
+    return queues[name];
 };
