12788c5bf2c071fba3f4b5374af73f57053e2333
vdiez <diez@outlook.com>
2019-12-02 17:06:02 +0100

    updated winston added size method added change size method

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-let queues = {};
 let winston = require('winston');
 
 function Queue(limit) {
-    this.queues = new Array(limit).fill(1).map((_, idx) => idx);
+    this.slots = new Array(limit).fill(1).map((_, idx) => idx);
     this.dispatcher = 0;
-    this.tasks = 0;
+    this.counter = 0;
 }
 
-Queue.prototype.run = function (task) {
-    let self = this;
-    self.tasks++;
-    winston.silly((task.name || task) + " added. Pending tasks: " + self.tasks);
-    return new Promise((resolve, reject) => {
-        self.dispatcher = Promise.resolve(self.dispatcher)
-            .then(function () {
-                return new Promise(function (resolve_dispatcher) {
-                    return Promise.race(self.queues)
-                        .then(function (queue) {
-                            self.queues[queue] = Promise.resolve(self.queues[queue])
-                                .then(function () {
-                                    return task();
-                                })
-                                .catch(err => {
-                                    winston.error((task.name || task) + " failed on queue " + queue + ". Error: ", err);
-                                    reject(err);
-                                })
-                                .then(result => {
-                                    self.tasks--;
-                                    winston.silly((task.name || task) + " completed on queue " + queue + ". Pending tasks: " + self.tasks);
-                                    resolve(result);
-                                    return queue;
-                                });
-                            resolve_dispatcher();
-                        })
-                });
-            })
-    });
+Queue.prototype = {
+    run(task) {
+        this.counter++;
+        winston.silly((task.name || task) + " added. Pending tasks: " + this.counter);
+        return new Promise((resolve, reject) => {
+            this.dispatcher = Promise.resolve(this.dispatcher)
+                .then(() => new Promise(resolve_dispatcher => Promise.race(this.slots)
+                    .then(slot => {
+                        this.slots[slot] = Promise.resolve(this.slots[slot])
+                            .then(() => task())
+                            .catch(err => {
+                                winston.error((task.name || task) + " failed on slot " + slot + ". Error: ", err);
+                                reject(err);
+                            })
+                            .then(result => {
+                                this.counter--;
+                                winston.silly((task.name || task) + " completed on slot " + slot + ". Pending tasks: " + this.counter);
+                                resolve(result);
+                                return slot;
+                            });
+                        resolve_dispatcher();
+                    })))
+        });
+    },
+    size() {
+        return this.slots.length;
+    },
+    set_size(limit) {
+        for (let i = this.slots.length; i < limit; i++) this.slots.push(i);//if limit has been increased
+        while (this.slots.length > limit) this.slots.pop();//if limit has been reduced
+    }
 };
 
-module.exports = function(name, limit) {
+let queues = {};
+module.exports = (name, limit) => {
     if (typeof limit === "undefined") {
         if (typeof name === "number") {
             limit = name;
@@ -54,5 +56,6 @@ module.exports = function(name, limit) {
         if (isNaN(limit) || limit < 1) throw "Incorrect parameters";
         queues[name] = new Queue(limit);
     }
+    else if (!isNaN(limit) && limit != queues[name].size()) queues[name].set_size(limit);
     return queues[name];
 };
