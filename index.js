7a9ac5df8914b1e7fead28c1d5709ab98696badf
vdiez <diez@outlook.com>
2020-09-18 17:04:45 +0200

    added option to create unnamed limits

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-let winston = require('winston');
-
 function Queue(limit) {
-    this.slots = new Array(limit).fill(1).map((_, idx) => idx);
-    this.dispatcher = 0;
+    this.slots = new Array(limit).fill(1).map((_, idx) => Promise.resolve(idx));
+    this.dispatcher = Promise.resolve();
     this.counter = 0;
 }
 
 Queue.prototype = {
     run(task) {
         this.counter++;
-        winston.silly((task.name || task) + " added. Pending tasks: " + this.counter);
         return new Promise((resolve, reject) => {
-            this.dispatcher = Promise.resolve(this.dispatcher)
+            this.dispatcher = this.dispatcher
                 .then(() => new Promise(resolve_dispatcher => Promise.race(this.slots)
                     .then(slot => {
-                        this.slots[slot] = Promise.resolve(this.slots[slot])
+                        this.slots[slot] = this.slots[slot]
                             .then(() => task())
-                            .catch(err => {
-                                winston.error((task.name || task) + " failed on slot " + slot + ". Error: ", err);
-                                reject(err);
-                            })
+                            .catch(err => reject(err))
                             .then(result => {
                                 this.counter--;
-                                winston.silly((task.name || task) + " completed on slot " + slot + ". Pending tasks: " + this.counter);
                                 resolve(result);
                                 return slot;
                             });
@@ -34,8 +27,8 @@ Queue.prototype = {
         return this.slots.length;
     },
     set_size(limit) {
-        for (let i = this.slots.length; i < limit; i++) this.slots.push(i);//if limit has been increased
-        while (this.slots.length > limit) this.slots.pop();//if limit has been reduced
+        for (let i = this.slots.length; i < limit; i++) this.slots.push(Promise.resolve(i)); //limit increased
+        while (this.slots.length > limit) this.slots.pop().catch(() => {});                  //limit reduced
     }
 };
 
@@ -44,18 +37,18 @@ module.exports = (name, limit) => {
     if (typeof limit === "undefined") {
         if (typeof name === "number") {
             limit = name;
-            name = '';
+            name = undefined;
         }
-        else if (typeof name === "undefined") name = '';
-        if (typeof name !== "string") throw "Incorrect parameters";
     }
-    else limit = +limit;
 
-    name = name + '';
-    if (!queues.hasOwnProperty(name)) {
-        if (isNaN(limit) || limit < 1) throw "Incorrect parameters";
-        queues[name] = new Queue(limit);
+    if (typeof name === "string") {
+        if (!queues.hasOwnProperty(name)) {
+            if (typeof limit !== "number" || limit < 1) throw "Incorrect limit";
+            queues[name] = new Queue(limit);
+        }
+        else if (typeof limit === "number" && limit > 0 && limit !== queues[name].size()) queues[name].set_size(limit);
+        return queues[name];
     }
-    else if (!isNaN(limit) && limit != queues[name].size()) queues[name].set_size(limit);
-    return queues[name];
+    if (typeof limit !== "number" || limit < 1) throw "Incorrect limit";
+    return new Queue(limit);
 };
